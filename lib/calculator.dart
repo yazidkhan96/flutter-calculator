d3bab423fe7456ced394eb76d8daf6329cd8d4a5
yazid akbar <mryazidkhan@gmail.com>
2020-08-15 13:18:26 +0700

    flutter calculator

---
+++ lib/calculator.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator_demo/display.dart';
+import 'package:flutter_calculator_demo/key-controller.dart';
+import 'package:flutter_calculator_demo/key-pad.dart';
+import 'package:flutter_calculator_demo/processor.dart';
+
+class Calculator extends StatefulWidget {
+
+	Calculator({ Key key }) : super(key: key);
+
+	@override
+	_CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+
+	String _output;
+
+	@override
+	void initState() {
+		
+		KeyController.listen((event) => Processor.process(event));
+		Processor.listen((data) => setState(() { _output = data; }));
+		Processor.refresh();
+		super.initState();
+	}
+
+	@override
+	void dispose() {
+
+		KeyController.dispose();
+		Processor.dispose();
+		super.dispose();
+	}
+
+	@override
+	Widget build(BuildContext context) {
+
+		Size screen = MediaQuery.of(context).size;
+
+		double buttonSize = screen.width / 4;
+		double displayHeight = screen.height - (buttonSize * 5) - (buttonSize);
+	
+		return Scaffold(
+			backgroundColor: Colors.white,
+			body: Column(
+				
+				mainAxisAlignment: MainAxisAlignment.center,
+				children: <Widget>[
+
+					Display(value: _output, height: displayHeight),
+					KeyPad()
+				]
+			),
+		);
+	}
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_calculator_demo/calculator.dart';
+
+void main() async {
+	
+	WidgetsFlutterBinding.ensureInitialized();
+
+	await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+	runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+
+	@override
+	Widget build(BuildContext context) {
+
+		return MaterialApp(
+			debugShowCheckedModeBanner: false,
+			theme: ThemeData(primarySwatch: Colors.teal),
+			home: Calculator(),
+		);
+	}
+}
